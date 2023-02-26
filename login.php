ec39e8252ec843676dcdaeb1f52e9b7238d2e918
TuanKietIT <ngokiet68@gmail.com>
2023-02-26 08:24:23 +0700

    Them form mat khau

--- login.php
+++ login.php
@@ -1,15 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <form action="" method="POST">
         <label for="">Username</label>
-        <input type="text" name="usename">
+        <input type="text" name="txtUseName">
+        <label for="">Password</label>
+        <input type="password" name="txtmatkhau">
     </form>
 </body>
+
 </html>
